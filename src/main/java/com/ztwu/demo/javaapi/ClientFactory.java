350af6da3091b8f036fc5320918d0504adb450d8
吴彰涛 <1182517419@qq.com>
2019-07-18 10:13:12 +0800

    优化

--- src/main/java/com/ztwu/demo/javaapi/ClientFactory.java
+++ src/main/java/com/ztwu/demo/javaapi/ClientFactory.java
@@ -26,10 +26,17 @@ public class ClientFactory {
             //设置集群名称(配置信息)
             Settings settings = Settings.builder()
                     .put("cluster.name", "edmp_bigdata_cluster")
+                    //忽视连接集群时名字验证
+                    .put("client.transport.ignore_cluster_name", true)
+                    //ping 一个节点时等待时间 默认5秒
+                    .put("client.transport.ping_timeout", "5s")
+                    //多久采样 ping / 节点列表和连接
+                    .put("client.transport.nodes_sampler_interval", "5s")
                     .build();
+
             //创建client(添加连接地址)
             transportClient = new PreBuiltTransportClient(settings)
-                    .addTransportAddress(new TransportAddress(InetAddress.getByName("*.*.*.*"), 9300));
+                    .addTransportAddress(new TransportAddress(InetAddress.getByName("172.31.17.2"), 9300));
         } catch (Exception e) {
             e.printStackTrace();
         }
